d7631ce7e10f63fe231482a5f2904107adefadad
Jacob Duijzer <jacob@duijzer.com>
2020-01-30 22:17:11 +0100

    android liner

---
+++ src/BestBeforeApp.Android/PreserveAttribute.cs
@@ -0,0 +1,9 @@
+using System;
+namespace Android.Runtime
+{
+    public sealed class PreserveAttribute : System.Attribute
+    {
+        public bool AllMembers;
+        public bool Conditional;
+    }
+}
